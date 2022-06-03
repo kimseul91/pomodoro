76a4730dfdc05b1b77399138225b01fe6c28edcc
kimseul91 <59694254+kimseul91@users.noreply.github.com>
2022-06-02 22:47:42 -0700

    button created to set start time

--- src/pomodoro/timer/Timer.js
+++ src/pomodoro/timer/Timer.js
@@ -3,15 +3,18 @@ import {useState, useEffect} from 'react';
 function Timer(props) {
     const [startTime, setStartTime] = useState(0);
 
-    setInterval(() => {
+    function set_Time () {
         let time = new Date();
         setStartTime(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
-
-    }, 1000);
+    }
+    
     return (
         <div>
-            timer section {startTime}
-            and current time is {props.currtime}
+            timer section {startTime} and current time is {props.currtime}
+            <div>
+                
+            </div>
+            <button onClick={() => {set_Time()}}> Start </button>
         </div>
     )
 
